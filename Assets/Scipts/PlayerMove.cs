ea858ba22ab6910d219ee2ffe0bdb2286047bd1c
MR_Kotuk <mrkotuk333@gmail.com>
2023-12-11 15:52:07 +0100

    0.5 Done Move Animations

--- Assets/Scipts/PlayerMove.cs
+++ Assets/Scipts/PlayerMove.cs
@@ -13,10 +13,8 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float _powerJumpPlayer;
     [SerializeField] private float _maxSpeedPlayer, _maxJumpPlayer;
 
-    private List<string> _animTypeNames = new List<string> { "isWalk", "isWalkBack", "isRun", "isRunBack", "isWait"};
     private float _dirX, _dirY;
     private bool isJump;
-    private string _typeMove = "TypeMove";
 
 
     private void OnValidate()
@@ -36,34 +34,11 @@ public class PlayerMove : MonoBehaviour
         _dirX = _moveJoystick.Horizontal;
         _dirY = _moveJoystick.Vertical;
 
-        transform.localPosition += transform.forward * _dirY * _speedPlayer;
-        transform.localPosition += transform.right * _dirX * _speedPlayer;
+        //transform.localPosition += transform.forward * _dirY * _speedPlayer;
+        //transform.localPosition += transform.right * _dirX * _speedPlayer;
 
-        Debug.Log($"{_dirY}, {_anim.GetFloat(_typeMove)}");
-        if (_dirY < 0.5f && _dirY > 0)
-            AnimationState(_animTypeNames[0], 0);
-        else if (_dirY >= 0.5 && _dirY > 0)
-            AnimationState(_animTypeNames[2], 0);
-        else if (_dirY > -0.5f && _dirY < 0)
-            AnimationState(_animTypeNames[1], 0);
-        else if (_dirY <= -0.5f && _dirY < 0)
-            AnimationState(_animTypeNames[3], 0);
-        else
-            AnimationState(_animTypeNames[4], 0);
-    }
-
-    private void AnimationState(string animName, int numTypeMove)
-    {
-        for (int i = 0; i < _animTypeNames.Count; i++)
-        {
-            if (_animTypeNames[i] == animName)
-            {
-                _anim.SetBool(animName, true);
-                _anim.SetFloat(_typeMove, numTypeMove);
-            }
-            else
-                _anim.SetBool(_animTypeNames[i], false);
-        }
+        _anim.SetFloat("MoveX", _dirX);
+        _anim.SetFloat("MoveY", _dirY);
     }
     public void OnJump()
     {
@@ -71,14 +46,6 @@ public class PlayerMove : MonoBehaviour
             _rb.AddForce(new Vector3(0, 1, 0) * _powerJumpPlayer);
     }
     
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.transform.position.y < gameObject.transform.position.y)
-            isJump = true;
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.transform.position.y < gameObject.transform.position.y)
-            isJump = false;
-    }
+    private void OnCollisionStay(Collision collision) => isJump = true;
+    private void OnCollisionExit(Collision collision) => isJump = false;
 }

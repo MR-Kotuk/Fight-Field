d202900a7c9b80dbad46252135477a3af552ebc0
MR_Kotuk <mrkotuk333@gmail.com>
2024-02-01 15:09:53 +0100

    3.6 New Test Map

--- Library/Bee/Android/Prj/Mono2x/Gradle/launcher/build/generated/source/buildConfig/release/com/ACTEN/BankHack/BuildConfig.java
+++ Library/Bee/Android/Prj/Mono2x/Gradle/launcher/build/generated/source/buildConfig/release/com/ACTEN/BankHack/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.ACTEN.BankHack";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "2.8";
+  public static final String VERSION_NAME = "3.5";
 }
